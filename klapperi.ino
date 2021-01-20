6db467269fc889fecd52ecfa27ae8122504629d7
P3rk3l3s <66990692+P3rk3l3s@users.noreply.github.com>
2021-01-20 09:29:47 +0200

    Add files via upload

--- klapperi.ino
+++ klapperi.ino
@@ -1,25 +1,22 @@
-int soundSensor = 2;
-int pinLEDred = 3;
-int pinLEDgreen = 4;
-int pinLEDblue = 5;
-int val = 0;
-boolean condition = 0;
+const int soundSensor = 2; /* nimet eri pinneille */
+const int pinLEDred = 3;
+const int pinLEDgreen = 4;
+const int pinLEDblue = 5;
+boolean condition = false;
 
 void setup(){
   Serial.begin(9600);
-    pinMode(pinLEDred, OUTPUT);
+    pinMode(pinLEDred, OUTPUT); /* määritykset mitä eri pinnit tekevät */
     pinMode(pinLEDgreen, OUTPUT);
     pinMode(pinLEDblue, OUTPUT);
     pinMode(soundSensor, INPUT);
 }
 
 void loop(){
-  int sensorValue = digitalRead(soundSensor);
-  if (sensorValue == HIGH)
-  {
-    condition = !condition;
-    digitalWrite(pinLEDred, condition);
-    Serial.println (" Ääni tulee ");
-  }
-  delay (0);
+    int sensorValue = digitalRead(soundSensor); /* määrittää äänisensorin */
+      if (sensorValue == HIGH){ /* lukee äänisensoria */
+      condition = !condition; /* kääntää arvon */
+      digitalWrite(pinLEDred, condition); /* "pinLEDred" voi vaihtaa tarvittaessa muihin eri pinneihin */
+        Serial.println (" Ääni kuuluu ");  /* printtaa serialiin jos kuuluu ääni */
+     }
 }
